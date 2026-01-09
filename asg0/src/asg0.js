90098f44fc8fae0a4ee59d2a83bca4c878678a22
Angela <annyu@ucsc.edu>
2026-01-08 21:56:10 -0800

    Added blue vector

--- asg0/src/asg0.js
+++ asg0/src/asg0.js
@@ -8,36 +8,40 @@ function main() {
     return false;
   }
   ctx = canvas.getContext('2d');
-  handleDrawEvent();
-}
-
-function getVector() {
-  var x1 = parseFloat(document.getElementById('xcoord').value);
-  if (isNaN(x1)){
-    x1 = 0;
-  }
-
-  var y1 = parseFloat(document.getElementById('ycoord').value);
-  if (isNaN(y1)){
-    y1 = 0;
-  }
-  return new Vector3([x1, y1, 0]);
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 function drawVector(v, color) {
-  var originX = canvas.width / 2;
-  var originY = canvas.height / 2;
-  var scale = 20;
-  var x = v.elements[0] * scale;
-  var y = v.elements[1] * scale;
   ctx.strokeStyle = color;
   ctx.beginPath();
+  var originX = canvas.width / 2;
+  var originY = canvas.height / 2;
+  var x = v.elements[0] * 20;
+  var y = v.elements[1] * 20;
+
   ctx.moveTo(originX, originY);
-  ctx.lineTo(originX + x, originY - y); 
+  ctx.lineTo(originX + x, originY - y);
   ctx.stroke();
+}
+function getVectors() {
+  var x1 = parseFloat(document.getElementById('xcoord').value);
+  if (isNaN(x1)) x1 = 0;
+  var y1 = parseFloat(document.getElementById('ycoord').value);
+  if (isNaN(y1)) y1 = 0;
+  var x2 = parseFloat(document.getElementById('xcoord2').value);
+  if (isNaN(x2)) x2 = 0;
+  var y2 = parseFloat(document.getElementById('ycoord2').value);
+  if (isNaN(y2)) y2 = 0;
+
+  var v1 = new Vector3([x1, y1, 0]);
+  var v2 = new Vector3([x2, y2, 0]);
+  return [v1, v2];
 }
 function handleDrawEvent() {
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  var v1 = getVector();
+
+  var [v1, v2] = getVectors();
   drawVector(v1, 'red');
+  drawVector(v2, 'blue');
 }
