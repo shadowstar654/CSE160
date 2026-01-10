fc50019d93edd0434027062d71289b2d0bdfc342
Angela <annyu@ucsc.edu>
2026-01-09 23:13:08 -0800

    4. Have HTML sliders for choosing the RGB color to paint

--- asg1/ColoredPoints.js
+++ asg1/ColoredPoints.js
@@ -48,11 +48,28 @@ function connectVariablesToGLSL(){
         return;
     }
 }
+let g_selectedColor=[1.0, 1.0, 1.0, 1.0];
+
+//Set up actions for the HTML UI elements
+function addActionsForHtmlUI(){
+    //Button Events (Shape Type)
+    document.getElementById('green').onclick = function() { g_selectedColor = [0.0, 1.0, 0.0, 1.0]; };
+    document.getElementById('red').onclick = function() { g_selectedColor = [1.0, 0.0, 0.0, 1.0]; };
+
+    //Slider Events
+    document.getElementById('redSlide').addEventListener('mouseup',   function() { g_selectedColor[0] = this.value/100; });
+    document.getElementById('greenSlide').addEventListener('mouseup',   function() { g_selectedColor[1] = this.value/100; });
+    document.getElementById('blueSlide').addEventListener('mouseup',   function() { g_selectedColor[2] = this.value/100; });
+
+}
 function main() {
   // Set up canvas and gl variables
   setupWebGL();
   // Set up GLSL shader programs and connect GLSL variables
   connectVariablesToGLSL();
+
+  //Set up actions for the HTML UI elements
+  addActionsForHtmlUI();
   // Register function (event handler) to be called on a mouse press
   canvas.onmousedown = click;
 
@@ -71,14 +88,18 @@ function click(ev) {
 
     // Store the coordinates to g_points array
     g_points.push([x, y]);
+
+    // Store a COPY of the currently selected color
+    //g_colors.push(g_selectedColor);
+    g_colors.push(g_selectedColor.slice());
     // Store the coordinates to g_points array
-    if (x >= 0.0 && y >= 0.0) {      // First quadrant
-        g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
-    } else if (x < 0.0 && y < 0.0) { // Third quadrant
-        g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
-    } else {                         // Others
-        g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
-    }
+    // if (x >= 0.0 && y >= 0.0) {      // First quadrant
+    //     g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
+    // } else if (x < 0.0 && y < 0.0) { // Third quadrant
+    //     g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
+    // } else {                         // Others
+    //     g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
+    // }
 
     // Draw every shape that is supposed to be in the canvas
     renderAllShapes();
